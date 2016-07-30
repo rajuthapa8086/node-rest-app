1eda594867862c60fdbd42054d8d7a2ec0b83bbd
rajuthapa8086 <raju.thapa.8086@gmail.com>
2016-07-31 04:00:45 +0545

    Bulk commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var rh = require('./lib/request-handler.js');
+
+rh.staticDir = __dirname + '/public';
+
+// Routes
+require('./routes.js')(rh);
+
+// Server
+http.createServer(function(req, res) {
+	rh.handle(req, res);
+}).listen(8000, function() {
+	console.log('Starting server at http://localhost:8000');
+});
--- routes.js
+++ routes.js
@@ -1,10 +1,59 @@
+'use strict';
+var fs = require('fs');
+var response = require('./lib/response.js');
+
+/**
+ * @var {string}
+ */
+var templatePath = __dirname + '/templates';
+
+/**
+ * @var {string}
+ */
+var dataFilePath = __dirname + '/data/contacts.json';
+
+/**
+ * @var {Array}
+ */
+var contacts = require('./data/contacts.json');
+
+/**
+ * @param {Object} rh
+ */
 module.exports = function(rh) {
 
   // GET /
   rh.get('/', function(req, res) {
-    res.writeHead(200, {
-      'Content-Type': 'text/plain'
+  	response.swigRender(res, templatePath + '/index.html');
+  });
+
+  // GET /contacts
+  rh.get('/contacts', function(req, res) {
+  	response.swigRender(res, templatePath + '/contacts/index.html', {
+      contacts: contacts
     });
-    res.end("Success");
+  });
+
+  // GET /contacts/add
+  rh.get('/contacts/add', function(req, res) {
+  	response.swigRender(res, templatePath + '/contacts/add.html', contacts);
+  });
+
+  rh.post('/contacts', function(req, res, data) {
+    var postData = data.data;
+    var date = new Date();
+    var id = [date.getFullYear(), date.getMonth(), date.getDate(),
+      date.getHours(), date.getMinutes(), date.getSeconds()].join('');
+    contacts.push({
+      id: id,
+      full_name: postData.full_name,
+      email: postData.email
+    });
+    fs.writeFile(dataFilePath, JSON.stringify(contacts), function (err) {
+      if (err) {
+        throw err;
+      }
+      response.redirect(res, '/contacts');
+    });
   });
 };
