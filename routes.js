575e1ddc251f41e5f288e627a8940252acff3330
rajuthapa8086 <raju.thapa.8086@gmail.com>
2016-08-01 04:15:09 +0545

    Contacts final

--- routes.js
+++ routes.js
@@ -17,30 +17,40 @@ var dataFilePath = __dirname + '/data/contacts.json';
  */
 var contacts = require('./data/contacts.json');
 
+function getContactById(id) {
+  for(var index in contacts) {
+    if (contacts[index].id == id) {
+      return contacts[index];
+    }
+  }
+  return null;
+}
+
 /**
  * @param {Object} rh
  */
 module.exports = function(rh) {
 
   // GET /
-  rh.get('/', function(req, res) {
+  rh.get('/', function(req, res, data) {
     response.swigRender(res, templatePath + '/index.html');
   });
 
   // GET /contacts
-  rh.get('/contacts', function(req, res) {
+  rh.get('/contacts', function(req, res, data) {
     response.swigRender(res, templatePath + '/contacts/index.html', {
       contacts: contacts
     });
   });
 
   // GET /contacts/add
-  rh.get('/contacts/add', function(req, res) {
+  rh.get('/contacts/add', function(req, res, data) {
     response.swigRender(res, templatePath + '/contacts/add.html');
   });
 
+  // POST /contacts
   rh.post('/contacts', function(req, res, data) {
-    var postData = data.data;
+    var postData = data.parsedBody;
     var date = new Date();
     var id = [date.getFullYear(), date.getMonth(), date.getDate(),
       date.getHours(), date.getMinutes(), date.getSeconds()].join('');
@@ -55,5 +65,83 @@ module.exports = function(rh) {
       }
       response.redirect(res, '/contacts');
     });
+  });
+
+  // GET /contacts/{id}
+  rh.get('/contacts/{id}', function(req, res, data) {
+    var contact = getContactById(data.params.id);
+    if (contact == null) {
+      console.log('404 DELETE ' + req.url);
+      res.writeHeader(404, {
+        'Content-Type': 'text/plain',
+      });
+      res.end();      
+    } else {
+      response.swigRender(res, templatePath + '/contacts/show.html', {
+        contact: contact
+      });      
+    }
+  });
+
+  // GET /contacts/{id}/edit
+  rh.get('/contacts/{id}/edit', function(req, res, data) {
+    var contact = getContactById(data.params.id);
+    if (contact == null) {
+      console.log('404 DELETE ' + req.url);
+      res.writeHeader(404, {
+        'Content-Type': 'text/plain',
+      });
+      res.end();      
+    } else {
+      response.swigRender(res, templatePath + '/contacts/edit.html', {
+        contact: contact
+      });      
+    }
+  });
+
+  // PUT /contacts/{id}
+  rh.put('/contacts/{id}', function(req, res, data) {
+    var contact = getContactById(data.params.id);
+    if (contact == null) {
+      console.log('404 DELETE ' + req.url);
+      res.writeHeader(404, {
+        'Content-Type': 'text/plain',
+      });
+      res.end();
+    } else {
+      var putData = data.parsedBody;
+
+      contacts[contacts.indexOf(contact)] = {
+        id: contact.id,
+        full_name: putData.full_name,
+        email: putData.email
+      };
+      fs.writeFile(dataFilePath, JSON.stringify(contacts), function (err) {
+        if (err) {
+          throw err;
+        }
+        response.redirect(res, '/contacts');
+      });
+    }
+  });
+
+  // DELETE /contacts/{id}
+  rh.delete('/contacts/{id}', function(req, res, data) {
+    var contact = getContactById(data.params.id);
+    if (contact == null) {
+      console.log('404 DELETE ' + req.url);
+      res.writeHeader(404, {
+        'Content-Type': 'text/plain',
+      });
+      res.end();
+    } else {
+      contacts.splice(contacts.indexOf(contact), 1);
+      fs.writeFile(dataFilePath, JSON.stringify(contacts), function (err) {
+        if (err) {
+          throw err;
+        }
+        response.redirect(res, '/contacts');
+      });
+    }
   });
 };
